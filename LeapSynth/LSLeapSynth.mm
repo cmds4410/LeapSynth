7b6701a4da8dbeb04d993925b13fb3f384595b07
Connor Smith <cmds4410@gmail.com>
2013-12-22 18:02:00 -0500

    “key press” to make a sound

--- LeapSynth/LSLeapSynth.mm
+++ LeapSynth/LSLeapSynth.mm
@@ -16,7 +16,10 @@ using namespace Tonic;
 @interface LSLeapSynth () <LeapListener>
 
 @property (strong, nonatomic) LeapController *leapController;
+@property (weak, nonatomic) TonicSynthManager *synthManager;
 
+- (void)configureLeapMotion;
+- (void)configureSynth;
 - (void)toggleTone;
 - (NSString *)stringForState:(LeapGestureState)state;
 
@@ -26,8 +29,8 @@ using namespace Tonic;
 
 - (void)run
 {
-    self.leapController = [[LeapController alloc] init];
-    [self.leapController addListener:self];
+    [self configureLeapMotion];
+    [self configureSynth];
 }
 
 - (void)dealloc
@@ -35,11 +38,57 @@ using namespace Tonic;
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
+#pragma mark - Configure
+
+- (void)configureLeapMotion
+{
+    self.leapController = [[LeapController alloc] init];
+    [self.leapController addListener:self];
+}
+
+- (void)configureSynth
+{
+    self.synthManager = [TonicSynthManager sharedManager];
+    
+    [self.synthManager startSession];
+}
+
 #pragma mark - Tonic
 
 - (void)toggleTone
 {
+    Synth mySynth = Synth();
     
+    [self.synthManager addSynth:mySynth forKey:@"mySynth"];
+    
+    ControlMetro metro = ControlMetro().bpm(200);
+    ControlGenerator freq = ControlRandom().trigger(metro).min(0).max(1);
+    
+    //Tonic is a collection of signal generators and processors
+    TriangleWave tone1 = TriangleWave();
+    SineWave tone2 = SineWave().freq(mtof(60));
+    SineWave vibrato = SineWave().freq(10);
+    SineWave tremelo = SineWave().freq(1);
+    //    ControlMidiToFreq
+    Noise click = Noise();
+    
+    ADSR env = ADSR()
+    .attack(.0001)
+    .decay( 0.05 )
+    .sustain(0)
+    .release(0)
+    .doesSustain(false)
+    .trigger(1);
+    
+    //that you can combine using intuitive operators
+    Generator combinedSignal = (tone1 + tone2) * tremelo;
+    Generator triangle = tone1;
+    
+    Generator clickGen;
+    
+    clickGen = tone2 * env;
+    
+    mySynth.setOutputGen(clickGen);
 }
 
 #pragma mark - LeapListener
@@ -100,6 +149,7 @@ using namespace Tonic;
                 NSLog(@"Key Tap id: %d, %@, position: %@, direction: %@",
                       keyTapGesture.id, [self stringForState:keyTapGesture.state],
                       keyTapGesture.position, keyTapGesture.direction);
+                [self toggleTone];
                 break;
             }
             case LEAP_GESTURE_TYPE_SCREEN_TAP: {
